1957d6dcf93e798d37dbf63d6c4e25e3d49a1024
CTin <138020798+CTnn1001@users.noreply.github.com>
2024-10-27 11:04:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import streamlit as st 
 
+st.title("Hello, my first app") 
+st.write("Day la ung dung dau tien cua toi")
+
+df = pd.read_csv("lettuce_dataset.csv") 
+st.line_chare(df)
