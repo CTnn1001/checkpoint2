374c086c275f5f590c729581e74d6ada20890bc4
CTin <138020798+CTnn1001@users.noreply.github.com>
2024-10-27 11:06:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
+import streamlit as st
 import pandas as pd
-import streamlit as st 
+import numpy as np
 
-st.title("Hello, my first app") 
-st.write("Day la ung dung dau tien cua toi")
+st.title('Uber pickups in NYC')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
